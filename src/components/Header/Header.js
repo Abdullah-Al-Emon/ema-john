8beebfd1a16a0eeb72234abb905c36517d0ef868
abdullah al emon <abdullahemon905@gmail.com>
2022-11-03 19:36:10 +0600

    third commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="" />
             <div>
-                <Link to="/">Sho</Link>
+                <Link to="/">Shop</Link>
                 <Link to="/orders">Orders</Link>
                 <Link to="/inventory">Inventory</Link>
                 <Link to="/about">About</Link>
